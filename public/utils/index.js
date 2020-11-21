f0bc2394f813c60d623960b08a7acb4bc5bc7bc7
sjrusso8 <sjrusso8@gmail.com>
2020-11-21 14:35:28 -0500

    complete working predicition

--- public/utils/index.js
+++ public/utils/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import * as tf from "@tensorflow/tfjs";
 import * as loader from "./loader";
 import { preprocess } from "./utils";
@@ -21,8 +20,8 @@ class mnistCNNModel {
         tf.tidy(() => {
             try {
                 let preprocessd_img = preprocess(img);
-                let softmax = this.model.predict(preprocessd_img).dataSync();
-                let preds = Array.from(softmax).map(n => parseFloat(n.toPrecision(4)));
+                let preds = this.model.predict(preprocessd_img).dataSync();
+                // let preds = Array.from(softmax).map(n => parseFloat(n.toPrecision(4)));
                 console.log(preds);
 
                 // return this.model.predict(preprocessd_img).dataSync();
